25b1879b9a33c0162d7dba815815eac38eddbb19
MatthewTLackey <lackey.matt@gmail.com>
2013-11-15 15:22:59 -0500

    Index and show functioning

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,29 @@
 class RestaurantsController < ApplicationController
+
+def index
+  @restaurants = Restaurant.all
+end
+
+def new
+
+end
+
+def create
+
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SeatYourself::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root :to => "restaurants#index"
   resources :users
   resources :restaurants
   resources :reservations
