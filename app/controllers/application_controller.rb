bbc6d445f52e0bf6f5b441b395cc3608861c2b0b
Erich Welz <erichwelz@gmail.com>
2013-11-17 16:57:05 -0500

    Adds views for making a reservation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id] # ||= (or equals) assigns item if item currently nil
+    
   end
 
   helper_method :current_user
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,12 +2,20 @@ class ReservationsController < ApplicationController
   before_filter :ensure_logged_in
 
   def new
+    @reservation = Reservation.new
   end
 
   def show
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redire_to restaurant_path(@restaurant), notice: "Reservation submitted."
+    else
+      render :action => :show
+    end
   end
 
   def index
