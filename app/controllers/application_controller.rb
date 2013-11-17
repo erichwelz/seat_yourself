9582638c7d0416ed8a1f7bbc65118c41f6e41b64
Erich Welz <erichwelz@gmail.com>
2013-11-17 16:06:26 -0500

    Ensured can't view Reservation model unless logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in"
+      redirect_to login_path
+    end
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] # ||= (or equals) assigns item if item currently nil
+  end
+
+  helper_method :current_user
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
+
+
---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,25 @@
+class ReservationsController < ApplicationController
+  before_filter :ensure_logged_in
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
