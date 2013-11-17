7d35628a27860091f9dceb2e9ab73848cf4c2cdc
Erich Welz <erichwelz@gmail.com>
2013-11-17 15:11:27 -0500

    Re-add username to users, configured Sorcery to register and login-in log-out users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115222205) do
+ActiveRecord::Schema.define(version: 20131117200935) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20131115222205) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
 end
