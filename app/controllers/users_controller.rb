28986e03bdaf81aacd088666a4619cd5e370cb2f
Erich Welz <erichwelz@gmail.com>
2013-11-18 14:30:30 -0500

    Reservations skeleton show page -show Restaurants

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,11 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+
+    if current_user
+      @reservations = @user.reservations
+    end
   end
 
   # GET /users/new
